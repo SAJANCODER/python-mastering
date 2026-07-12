7509d7c81538b06c5c43d7bd0f2c8e21abdd1c8c
SAJAN D <sajanneyveli@gmail.com>
2026-07-12 21:55:06 +0530

    sliding window

--- linkedlist/longest_sub_string.py
+++ linkedlist/longest_sub_string.py
@@ -1,11 +1,18 @@
 #longest sub string
-a = input("Enter the string: ").split()
-seen = []
-for i in a :
-    print(seen)
-    if i in seen:
-        seen.remove(i)
-        seen.append(i)
-    else:
-        seen.append(i)
-print(len(seen))
+a = input("Enter a sring:").strip()
+left = 0
+seen= set()
+max1 = 0
+start = 0
+for right in range(len(a)):
+    while a[right] in seen:
+        
+        seen.remove(a[left])
+        left+=1
+    seen.add(a[right])
+    current_window = right-left+1
+    if current_window>max1:
+        max1=current_window
+        start=left
+print(max1)
+print("Substring is :",a[start:start+max1])
